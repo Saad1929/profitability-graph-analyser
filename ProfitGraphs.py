c6053b0acd40ade16cdcae25eab173e681e0d91a
Saad1929 <saad2baig123@gmail.com>
2023-07-08 23:24:55 +0100

    Read Data Function Started

--- ProfitGraphs.py
+++ ProfitGraphs.py
@@ -2,3 +2,11 @@ import matplotlib.pyplot as plt
 plt.style.use("seaborn")
 
 f = open("company-sales.csv", "r")
+
+def read_data_from_csv(filename):
+    company_sales_data = {}
+
+    line = filename.readline()[:-1]
+    while line != "":
+        list_line = line.split(sep=",")
+        assert len(list_line) == 9, "There is a line in the file which doesn't contain the correct sales data!"
