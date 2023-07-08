59259dc62973c90649a69e3424b07396b82af5e5
Saad1929 <saad2baig123@gmail.com>
2023-07-08 23:32:50 +0100

    Read File Function Try Block

--- ProfitGraphs.py
+++ ProfitGraphs.py
@@ -13,3 +13,9 @@ def read_data(filename):
     while line != "":
         list_line = line.split(sep=",")
         assert len(list_line) == 9, "There is a line in the file which doesn't contain the correct sales data!"
+        try: 
+            first_line = int(list_line[0])
+            if type(first_line) == type(1):
+                for item in list_line:
+                    month_number = list_line[0]
+                    company_sales_data[int(month_number)] = [float(value) for value in list_line[1:]]
