2af85ec4948917074256a0970301e1614dfa4a1e
Saad1929 <saad2baig123@gmail.com>
2023-07-09 13:49:41 +0100

    Plot Specific Product

--- ProfitGraphs.py
+++ ProfitGraphs.py
@@ -119,3 +119,17 @@ def plot_histogram_total_units(data_per_month):
     plt.xlabel("Total Number Of Units Sold")
     plt.show()
 
+def plot_toothpaste_sales(data_per_month):
+    months = {1 : "Januray",
+              2 : "February",
+              3 : "March",
+              4 : "April",
+              5 : "May",
+              6 : "June",
+              7 : "July",
+              8 : "August",
+              9 : "September",
+              10 : "October",
+              11 : "November",
+              12 : "December"}
+    
